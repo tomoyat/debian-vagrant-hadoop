17ba4315cdcea1783319a9125d8686b2a224368a
tomoyat <20t.toya@gmail.com>
2014-02-01 19:11:23 +0900

    added base cookbook

---
+++ cookbooks/base/recipes/default.rb
@@ -0,0 +1,36 @@
+#
+# Cookbook Name:: base
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+#time zone
+bash "set timezone jst" do
+  code <<-EOC
+    sudo cp -p /usr/share/zoneinfo/Japan /etc/localtime
+EOC
+end
+
+bash "apt-get update" do
+  code <<-EOC
+    sudo apt-get update
+EOC
+end
+
+package "git" do
+  action :install
+end
+
+package "zsh" do
+  action :install
+end
+
+package "screen" do
+  action :install
+end
+
+package "vim" do
+  action :install
+end
